9b4cb017d95dcf4aab46999e8d51a784bc4f817b
hrantgartley <doyouevenjump@gmail.com>
2024-02-01 11:14:38 -0600

    changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,25 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-undef */
+/* global $ */
 $(function () {
-    $("img").draggable()
+    let wowCount = 0
+    let wowMsg
+
+    $("#dogePic").draggable({
+        scroll: false,
+
+        start: function () {
+            wowCount++
+
+            if (wowCount <= 4) {
+                wowMsg = "your wow is weak"
+            } else if (wowCount <= 10) {
+                wowMsg = "your wow is mediocre"
+            } else {
+                wowMsg = "your wow is extraordinary"
+            }
+
+            $("#wowOutput").text(wowMsg + "(" + wowCount + ")")
+        },
+    })
+
+    $("#dogeDiv").draggable()
 })
