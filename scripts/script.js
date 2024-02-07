eda38efed8eb7ac7906f5778a151a090d529b4c3
hrantgartley <doyouevenjump@gmail.com>
2024-02-07 16:40:55 -0600

    change color based on wow

--- scripts/script.js
+++ scripts/script.js
@@ -11,10 +11,14 @@ $(function () {
 
             if (wowCount <= 4) {
                 wowMsg = "your wow is weak"
+                // set background color to red
+                $("body").css("background-color", "red")
             } else if (wowCount <= 10) {
                 wowMsg = "your wow is mediocre"
+                $("body").css("background-color", "yellow")
             } else {
                 wowMsg = "your wow is extraordinary"
+                $("body").css("background-color", "blue")
             }
 
             $("#wowOutput").text(wowMsg + "(" + wowCount + ")")
